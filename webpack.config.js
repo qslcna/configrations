6fdee3073511f7f236be6013929c99c8f2d1f35d
qs <qslcna@outlook.com>
2018-05-22 23:39:53 +0800

    Create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: './app/app.js',
+  plugins: [
+    new CleanWebpackPlugin(['dist'])
+  ],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  
+};

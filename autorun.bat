626ace71d99be157feb8b73e7d015f17cd8c22de
qs <qslcna@outlook.com>
2018-09-24 07:09:12 +0800

    Update autorun.bat

--- autorun.bat
+++ autorun.bat
@@ -1,8 +1,19 @@
 @echo off
 
+rem 将控制台编码由默认的gbk(936)修改为utf-8
 chcp 65001>nul
-doskey ls=dir /w
-doskey ll=dir
-doskey clear=cls
+
+rem 解决less(包括git log)乱码问题
+set LESSCHARSET=utf-8
+
+rem 解决java控制台乱码
+set JAVA_TOOL_OPTIONS=-Dfile.encoding=utf-8
+
+doskey ls=ls --color=auto $*
+doskey ll=ls -l --color=auto $*
+doskey la=ls -A --color=auto $*
+doskey l=ls -CF --color=auto $*
 
 doskey cnpm=npm --registry=https://registry.npm.taobao.org $*
+doskey pnpm=npm --proxy=http://localhost:1080 --https-proxy=http://localhost:1080 $*
+

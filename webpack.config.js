4e6cd1a20c7dc0ccc18f03546b0d01e2be405a2c
qs <qslcna@outlook.com>
2018-11-03 22:34:18 +0800

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,19 @@ const path = require('path');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
-  entry: './app/app.js',
-  devtool: 'source-map',
-  plugins: [
-    new CleanWebpackPlugin(['dist'])
-  ],
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  
+    entry: './app/app.js',
+    devtool: 'source-map',
+    plugins: [
+        new CleanWebpackPlugin(['dist'])
+    ],
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    resolve:{
+        alias:{
+            app: path.resolve( __dirname, "app" )
+        }
+    }
+
 };
